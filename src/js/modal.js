58e6b52e8cf8c7b90eb3dec8293b073c7b610305
Cassandder <nocte.vindicem@gmail.com>
2021-05-25 11:10:04 +0300

    modal

---
+++ src/js/modal.js
@@ -0,0 +1,19 @@
+
+(() => {
+  const refs = {
+    openModalBtns: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtns.forEach(e => {
+    e.addEventListener('click', toggleModal);
+  });
+
+  // refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
